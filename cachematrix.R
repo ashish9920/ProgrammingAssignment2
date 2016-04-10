0adb9e727498f7fcdedb5dd23b6b950846d284ed
Ashish Jadhao <ash9920jad@gmail.com>
2016-04-11 02:25:57 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,8 +4,40 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        cachedInv <- NULL
+        
+         set <- function(userValue = matrix()) {
+    x <<- userValue 
+    cachedInv <<- NULL
+  }
+  get <- function() x
+  
+  setInverse <- function(invVal) {
+    cachedInv <<- invVal 
+    return(cachedInv)
+  }
+  getInverse  <- function() cachedInv
+  list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
+cacheSolve <- function(x=makeCacheMatrix(1:4, nrow=2, ncol=2), ...) {
+  
+  calculatedInverse <- x$getInverse() 
+  
+  if(!is.null(calculatedInverse) && is.matrix(calculatedInverse)) { 
+    return(calculatedInverse)
+  }
+  matrixToSolve <- x$get()  
+  
+  calculatedInverse <- tryCatch({ 
+    solve(matrixToSolve)
+  }
+  
+  ## whatever the case, set the value of the inverse (NULL if something went wrong)
+  message("value of inverse:") 
+  x$setInverse(calculatedInverse)
+}
+
+
 
 
 ## Write a short comment describing this function
